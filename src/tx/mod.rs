8e958300b09ddc637f385b9901aebaa331c5dab7
James Prestwich <james@prestwi.ch>
2020-03-16 15:45:19 -0700

    refactor: simplify VarInt deser

--- src/tx/mod.rs
+++ src/tx/mod.rs
@@ -7,15 +7,29 @@ pub struct VarInt(pub u64, pub u8);   // number and byte-length
 
 impl VarInt {
     pub fn new(number: u64) -> Self {
-        let byte_len = match number {
-            0..=0xfc => 1,
-            0xfd..=0xffff => 3,
-            0x10000..=0xffffff => 5,
-            _ => 9
-        };
+        let byte_len = VarInt::byte_len(number);
         VarInt(number, byte_len)
     }
+
+    pub fn byte_len(number: u64) -> u8 {
+        match number {
+            0..=0xfc => 1,
+            0xfd..=0xffff => 3,
+            0x10000..=0xffff_ffff => 5,
+            _ => 9
+        }
+    }
+
+    pub fn len_from_prefix(number: u8) -> usize {
+        match number {
+            0..=0xfc => 1,
+            0xfd => 3,
+            0xfe => 5,
+            0xff => 9
+        }
+    }
 }
+
 
 impl Serialize for VarInt {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -34,44 +48,29 @@ impl Serialize for VarInt {
     }
 }
 
-
 impl<'de> Deserialize<'de> for VarInt {
     fn deserialize<D>(deserializer: D) -> Result<VarInt, D::Error>
     where
         D: Deserializer<'de>
     {
         let s: &str = Deserialize::deserialize(deserializer)?;
-        let result = utils::deserialize_hex(s).map_err(|v| de::Error::custom(v))?;
-        match (result.get(0), result.len()) {
-            (None, _) => Err(de::Error::custom("Null bytestring in VarInt deser")),
-            (Some(0..=0xfc), 1) => Ok(VarInt(result[0] as u64, 1)),
-            (Some(0xfd), 3) => {
-                let num: u64 =
-                    result[1] as u64
-                    + (result[2] as u64) << 1;
-                Ok(VarInt(num, 3))
-            },
-            (Some(0xfe), 5) => {
-                let num: u64 =
-                    result[1] as u64
-                    + (result[2] as u64) << 1
-                    + (result[3] as u64) << 2
-                    + (result[4] as u64) << 3;
-                Ok(VarInt(num, 5))
-            },
-            (Some(0xff), 9) => {
-                let num: u64 =
-                    result[1] as u64
-                    + (result[2] as u64) << 1
-                    + (result[3] as u64) << 2
-                    + (result[4] as u64) << 3
-                    + (result[5] as u64) << 4
-                    + (result[6] as u64) << 5
-                    + (result[7] as u64) << 6
-                    + (result[8] as u64) << 7;
-                Ok(VarInt(num, 9))
-            },
-            _ => Err(de::Error::custom("Unknown Error in VarInt deser"))
+        let bytes = utils::deserialize_hex(s).map_err(de::Error::custom)?;
+
+        let expected_len = VarInt::len_from_prefix(bytes[0]);
+
+        if bytes.is_empty() || expected_len > bytes.len() {
+            return Err(de::Error::custom("Null bytestring in VarInt deser"));
+        }
+
+        match bytes[0] {
+            0..=0xfc => Ok(VarInt(bytes[0] as u64, 1)),
+            _ => {
+                let mut total = 0u64;
+                for (i, b) in bytes[1..expected_len].iter().enumerate() {
+                    total += (*b as u64) << i;
+                };
+                Ok(VarInt(total, expected_len as u8))
+            }
         }
     }
 }
