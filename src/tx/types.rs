aae289f18b14327ecdaf91b8270ed70dff141103
James Prestwich <james@prestwi.ch>
2020-03-17 18:27:52 -0700

    feature: len for vin/vout/etc, test for witness tx ser

--- src/tx/types.rs
+++ src/tx/types.rs
@@ -56,6 +56,10 @@ pub struct Script {
 }
 
 impl Script {
+    pub fn len(&self) -> usize {
+        self.length.0 as usize
+    }
+
     pub fn null() -> Self {
         Script::new(vec![])
     }
@@ -179,6 +183,19 @@ pub struct Witness{
     pub stack: Vec<WitnessStackItem>
 }
 
+impl Witness {
+    pub fn new(stack: Vec<WitnessStackItem>) -> Self {
+        Witness{
+            stack_items: VarInt::new(stack.len() as u64),
+            stack,
+        }
+    }
+
+    pub fn null() -> Self {
+        Witness::new(vec![])
+    }
+}
+
 impl Ser for Witness {
     fn serialized_length(&self) -> IOResult<usize> {
         let mut len = self.stack_items.serialized_length()?;
@@ -216,6 +233,10 @@ pub struct Vin{
 }
 
 impl Vin {
+    pub fn len(&self) -> usize {
+        self.length.0 as usize
+    }
+
     pub fn new(inputs: Vec<TxIn>) -> Self {
         Vin{
             length: VarInt::new(inputs.len() as u64),
@@ -291,6 +312,10 @@ impl Ser for Vout {
 }
 
 impl Vout {
+    pub fn len(&self) -> usize {
+        self.length.0 as usize
+    }
+
     pub fn new(outputs: Vec<TxOut>) -> Self {
         Vout{
             length: VarInt::new(outputs.len() as u64),
@@ -316,6 +341,21 @@ pub enum TxError{
 }
 
 impl Tx {
+    pub fn new_unsigned_witness(
+        version: u32,
+        vin: Vin,
+        vout: Vout,
+        locktime: u32
+    ) -> Self {
+        let n_ins = vin.len();
+        Tx::new(
+            version,
+            vin,
+            vout,
+            Some((0..n_ins).map(|_| Witness::null()).collect()),
+            locktime
+        ).unwrap()
+    }
 
     pub fn new(
         version: u32,
@@ -496,7 +536,7 @@ mod tests {
     }
 
     #[test]
-    fn it_assembles() {
+    fn it_assembles_legacy() {
         let numbers: Vec<u8> = (0u8..32u8).collect();
         let mut prevout_txid = [0u8; 32];
         prevout_txid.copy_from_slice(&numbers);
@@ -529,6 +569,48 @@ mod tests {
         // println!("serialized {:?}", &ser_str);
         assert_eq!(
             "0200000001000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1fddccbbaa16001411003300550077009900bb00dd00ff11003300558833883301780300000000000016001411003300550077009900bb00dd00ff110033005511223344",
+            ser_str
+        );
+        assert_eq!(
+            Tx::deserialize_hex(ser_str).unwrap(),
+            tx
+        );
+    }
+
+    #[test]
+    fn it_assembles_witness() {
+        let numbers: Vec<u8> = (0u8..32u8).collect();
+        let mut prevout_txid = [0u8; 32];
+        prevout_txid.copy_from_slice(&numbers);
+        let outpoint = Outpoint{
+            txid: prevout_txid,
+            idx: 2864434397
+        };
+        let spk = Script::new(
+            vec![0x00, 0x14, 0x11, 0x00, 0x33, 0x00, 0x55, 0x00, 0x77, 0x00, 0x99, 0x00, 0xbb, 0x00, 0xdd, 0x00, 0xff, 0x11, 0x00, 0x33, 0x00, 0x55]
+        );
+        let txin = TxIn{
+            outpoint,
+            script_sig: spk.clone(),
+            sequence: 0x33883388
+        };
+        let vin = Vin::new(vec![txin]);
+        let txout = TxOut{
+            value: 888u64,
+            script_pubkey: spk
+        };
+        let vout = Vout::new(vec![txout]);
+        let tx = Tx::new(
+            0x2u32,
+            vin,
+            vout,
+            Some(vec![Witness::null()]),
+            0x44332211u32
+        ).unwrap();
+        let ser_str = tx.serialize_hex().unwrap();
+        // println!("serialized {:?}", &ser_str);
+        assert_eq!(
+            "02000000000101000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1fddccbbaa16001411003300550077009900bb00dd00ff11003300558833883301780300000000000016001411003300550077009900bb00dd00ff11003300550011223344",
             ser_str
         );
         assert_eq!(
