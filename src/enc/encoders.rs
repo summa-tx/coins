749c91c6e8c011229a90679ecf563bd2c1e1d9de
James Prestwich <james@prestwi.ch>
2020-03-23 16:05:48 -0700

    refactor: move encoder properties to enc

---
+++ src/enc/encoders.rs
@@ -0,0 +1,95 @@
+use std::marker::PhantomData;
+
+use crate::{
+    enc::bases::{
+        Address,
+        EncodingError,
+        EncodingResult,
+        encode_base58,
+        decode_base58,
+        encode_bech32,
+        decode_bech32,
+    },
+    types::{
+        primitives::{PrefixVec},
+        script::{Script, ScriptType},
+    },
+};
+
+
+pub trait NetworkParams {
+    const HRP: &'static str;
+    const PKH_VERSION: u8;
+    const SH_VERSION: u8;
+}
+
+pub trait NetworkEncoder {
+    fn encode_address(a: Script) -> EncodingResult<Address>;
+    fn decode_address(addr: Address) -> EncodingResult<Script>;
+}
+
+pub struct AddressEncoder<P: NetworkParams>(PhantomData<P>);
+
+impl<P: NetworkParams> NetworkEncoder for AddressEncoder<P> {
+    fn encode_address(a: Script) -> EncodingResult<Address> {
+        match a.determine_type() {
+            ScriptType::PKH => {
+                Ok(Address::PKH(encode_base58(P::PKH_VERSION, &a.items())))
+            },
+            ScriptType::SH => {
+                Ok(Address::SH(encode_base58(P::SH_VERSION, &a.items())))
+            },
+            ScriptType::WSH => {
+                Ok(Address::WSH(encode_bech32(P::HRP, &a.items())?))
+            }
+            ScriptType::WPKH => {
+                Ok(Address::WPKH(encode_bech32(P::HRP, &a.items())?))
+            }
+            ScriptType::NonStandard => {
+                Err(EncodingError::UnknownScriptType)
+            }
+        }
+    }
+
+    fn decode_address(addr: Address) -> EncodingResult<Script> {
+        match &addr {
+            Address::PKH(s) => {
+                decode_base58(P::PKH_VERSION, s).map(|v| v.into())
+            },
+            Address::SH(s) => {
+                decode_base58(P::SH_VERSION, s).map(|v| v.into())
+            },
+            Address::WPKH(s) | Address::WSH(s) => {
+                decode_bech32(P::HRP, &s).map(|v| v.into())
+            }
+        }
+    }
+}
+
+pub struct Main;
+
+impl NetworkParams for Main {
+    const HRP: &'static str = "bc";
+    const PKH_VERSION: u8 = 0x00;
+    const SH_VERSION: u8 = 0x05;
+}
+
+pub struct Test;
+
+impl NetworkParams for Test {
+    const HRP: &'static str = "tb";
+    const PKH_VERSION: u8 = 0x6f;
+    const SH_VERSION: u8 = 0xc4;
+}
+
+pub struct Sig;
+
+impl NetworkParams for Sig {
+    const HRP: &'static str = "sb";
+    const PKH_VERSION: u8 = 0x7d;
+    const SH_VERSION: u8 = 0x57;
+}
+
+pub type MainnetEncoder = AddressEncoder<Main>;
+pub type TestnetEncoder = AddressEncoder<Test>;
+pub type SignetEncoder = AddressEncoder<Sig>;
--- src/nets/mod.rs
+++ src/nets/mod.rs
@@ -1,90 +1,19 @@
 use crate::{
-    types::{
-        primitives::{PrefixVec},
-        script::{Script, ScriptType},
-        // tx::{Transaction, WitnessTransaction},
-    },
-    enc::{
-        Address,
-        EncodingError,
-        EncodingResult,
-        encode_base58,
-        decode_base58,
-        encode_bech32,
-        decode_bech32,
+    enc::encoders::{
+        NetworkEncoder,
+        MainnetEncoder,
+        TestnetEncoder,
+        SignetEncoder,
     },
 };
 
 use std::marker::PhantomData;
 
-pub trait NetworkParams {
-    const HRP: &'static str;
-    const PKH_VERSION: u8;
-    const SH_VERSION: u8;
+// TODO: Add tx types here
+pub struct Network<Enc: NetworkEncoder> {
+    encoder: PhantomData<Enc>,
 }
 
-pub struct Network<P: NetworkParams>(PhantomData<P>);
-
-impl<P: NetworkParams> Network<P> {
-    pub fn encode_address(a: Script) -> EncodingResult<Address> {
-        match a.determine_type() {
-            ScriptType::PKH => {
-                Ok(Address::PKH(encode_base58(P::PKH_VERSION, &a.items())))
-            },
-            ScriptType::SH => {
-                Ok(Address::SH(encode_base58(P::SH_VERSION, &a.items())))
-            },
-            ScriptType::WSH => {
-                Ok(Address::WSH(encode_bech32(P::HRP, &a.items())?))
-            }
-            ScriptType::WPKH => {
-                Ok(Address::WPKH(encode_bech32(P::HRP, &a.items())?))
-            }
-            ScriptType::NonStandard => {
-                Err(EncodingError::UnknownScriptType)
-            }
-        }
-    }
-
-    pub fn decode_address(addr: Address) -> EncodingResult<Script> {
-        match &addr {
-            Address::PKH(s) => {
-                decode_base58(P::PKH_VERSION, s).map(|v| v.into())
-            },
-            Address::SH(s) => {
-                decode_base58(P::SH_VERSION, s).map(|v| v.into())
-            },
-            Address::WPKH(s) | Address::WSH(s) => {
-                decode_bech32(P::HRP, &s).map(|v| v.into())
-            }
-        }
-    }
-}
-
-pub type BitcoinMainnet = Network<Main>;
-pub type BitcoinRegtest = Network<Test>;
-pub type BitcoinSignet = Network<Signet>;
-
-pub struct Main;
-
-impl NetworkParams for Main {
-    const HRP: &'static str = "bc";
-    const PKH_VERSION: u8 = 0x00;
-    const SH_VERSION: u8 = 0x05;
-}
-
-pub struct Test;
-
-impl NetworkParams for Test {
-    const HRP: &'static str = "tb";
-    const PKH_VERSION: u8 = 0x6f;
-    const SH_VERSION: u8 = 0xc4;
-}
-
-pub struct Signet;
-
-impl NetworkParams for Signet {
-    const HRP: &'static str = "sb";
-    const PKH_VERSION: u8 = 0x7d;
-    const SH_VERSION: u8 = 0x57;
-}
+pub type BitcoinMainnet = Network<MainnetEncoder>;
+pub type BitcoinRegtest = Network<TestnetEncoder>;
+pub type BitcoinSignet = Network<SignetEncoder>;
