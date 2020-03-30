7c4647110ee411bd704745a6a54c3e68657f0886
James Prestwich <james@prestwi.ch>
2020-03-30 14:13:08 -0700

    chore: move network macro to prelude

--- bitcoin-wasm/src/prelude.rs
+++ bitcoin-wasm/src/prelude.rs
@@ -365,3 +365,33 @@ macro_rules! impl_encoder {
         }
     }
 }
+
+macro_rules! impl_network {
+    ($network_name:ident, $builder_name:ident, $encoder_name:ident)=> {
+        #[wasm_bindgen(inspectable)]
+        #[derive(Debug)]
+        pub struct $network_name;
+
+        #[wasm_bindgen]
+        impl $network_name {
+            pub fn tx_builder() -> $builder_name {
+                $builder_name::new()
+            }
+
+            pub fn encode_address(s: &[u8]) -> Result<Address, JsValue> {
+                $encoder_name::encode_address(s)
+
+            }
+
+            /// Attempt to decode a `RecipientIdentifier` from an `Address`.
+            pub fn decode_address(addr: Address) -> Result<js_sys::Uint8Array, JsValue> {
+                $encoder_name::decode_address(addr)
+            }
+
+            /// Attempt to convert a string into an `Address`.
+            pub fn wrap_string(s: String) -> Result<Address, JsValue> {
+                $encoder_name::wrap_string(s)
+            }
+        }
+    }
+}
