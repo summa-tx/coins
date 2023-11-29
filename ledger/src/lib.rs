1c724ed9ed9dca3d9893daf021f671358643a5f9
James <james@prestwi.ch>
2023-11-29 15:55:04 -0800

    fix: fmt

--- ledger/src/lib.rs
+++ ledger/src/lib.rs
@@ -9,10 +9,8 @@
     rustdoc::all
 )]
 #![deny(unused_must_use, rust_2018_idioms)]
-
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-
 
 /// APDU utilities.
 pub mod common;
